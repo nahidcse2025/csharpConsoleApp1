85bfe11917f7be5aa668746d632772c02e34d117
nahidcse2025 <nahid.edu.cse@gmail.com>
2025-02-21 07:13:09 +0600

    Add project files.

---
+++ csharpConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*Console.WriteLine("Hello, World!");
+int a = 10;
+int b = 20;
+int c = a + b;
+Console.WriteLine("Result is {0}+{1}={2}", a, b, c); */
+
