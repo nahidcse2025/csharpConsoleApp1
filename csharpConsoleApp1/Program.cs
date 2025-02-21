92db6261d4c15e659cf8ad1cc7c1efc83c05608f
nahidcse2025 <nahid.edu.cse@gmail.com>
2025-02-21 07:25:30 +0600

    double hello world

--- csharpConsoleApp1/Program.cs
+++ csharpConsoleApp1/Program.cs
@@ -2,8 +2,9 @@
 //Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Hello, World!");
-int a = 10;
+Console.WriteLine("Hello, World!");
+/*int a = 10;
 int b = 20;
 int c = a + b;
-Console.WriteLine("Result is {0}+{1}={2}", a, b, c);
+Console.WriteLine("Result is {0}+{1}={2}", a, b, c);*/
 
